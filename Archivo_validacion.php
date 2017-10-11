54a0b8faf508cc3a1308919563e827395382fda8
KarlosH2 <ill_carlos@hotmail.com>
2017-10-11 16:16:52 -0500

    test 1 carlos
    test 1 carlos

---
+++ Archivo_validacion.php
@@ -0,0 +1,18 @@
+<?php
+
+
+echo "Linea uno - 1 Carlos";
+
+echo "Linea uno - 2 Carlos";
+
+echo "Linea uno - 3 Carlos";
+
+echo "Linea uno - 4 Carlos";
+
+echo "Linea uno - 5 Carlos";
+
+
+
+
+
+?>
