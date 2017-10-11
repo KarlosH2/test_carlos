537c12c2649205c76ff47c9511a4eb8e2bc5d275
jhontanjh <gtzjhonatan@gmail.com>
2017-10-11 16:18:06 -0500

    Test Jh
    Test Jh

--- Archivo_validacion.php
+++ Archivo_validacion.php
@@ -6,12 +6,13 @@ echo "Linea uno - 1 Carlos";
 echo "Linea uno - 2 Carlos";
 
 echo "Linea uno - 3 Carlos";
-
+echo "Linea uno - 1 Jhonatan";
 echo "Linea uno - 4 Carlos";
 
 echo "Linea uno - 5 Carlos";
 
 
+echo "Linea uno - 2 Jhonatan";
 
 
 
